7c93f0d8c4b13b31f97a891c581e26075399a893
Maciej <mkowalcz@stevens.edu>
2024-07-31 17:51:45 -0400

    Switched from hashing to password encryption so passwords can be retrieved. Added functions for password retrieval

--- generatePassword.py
+++ generatePassword.py
@@ -1,7 +1,11 @@
 import random
 import string
-import bcrypt
+from cryptography.fernet import Fernet
 import os
+
+with open('.config', 'r') as file:
+    KEY = file.readline().strip()
+
 def generatePassword(length=10, numLowers=6, numCapitals=1, numDigits=2, numSpecialCharacters=1):
     if(type(length) is not int or type(numLowers) is not int or type(numCapitals) is not int or type(numDigits) is not int or type(numSpecialCharacters) is not int):
         raise TypeError("All inputs must be of type int")
@@ -22,27 +26,14 @@ def generatePassword(length=10, numLowers=6, numCapitals=1, numDigits=2, numSpec
 
     return password
 
-def hashPassword(password):
-    bytes = password.encode('utf-8')
-    hashedPassword = bcrypt.hashpw(bytes, bcrypt.gensalt())
-    return hashedPassword
-
-def hashAndSavePassword(username, platform, password):
-    hashPassword = hashPassword(password)
-    platformEntry = platform + ": "
-    entry = platformEntry + hashedPassword.decode('utf-8') + "\n"
-    f = open(username + ".txt", "a")
-    f.write(entry)
-    f.close()
-    return "Password successfully saved"
-
 def createAccount(username, masterPassword):
-    with open("master.txt") as file:
-        for line in file:
-            if username.lower() in line:
-                raise ValueError("Username already exists!")
-    f.close()
-    hashedPassword = hashPassword(masterPassword)
+    filename = username.lower() + ".txt"
+    try:
+        with open(filename, 'x') as file:
+            pass
+    except FileExistsError:
+        print(f"The account '{username.lower()}' already exists.")
+    hashedPassword = encryptPassword(masterPassword)
     usernameEntry = username.lower() + ": "
     entry = usernameEntry + hashedPassword.decode('utf-8') + "\n"
     f = open("master.txt", "a")
@@ -50,7 +41,7 @@ def createAccount(username, masterPassword):
     f.close()
     return "Account created: " + username.lower()
     
-def signIn(username, masterPassword):
+def validateUser(username, masterPassword):
     match = ""
     with open("master.txt") as file:
         for line in file:
@@ -59,13 +50,63 @@ def signIn(username, masterPassword):
         
     if match != "":
         userTuple = match.strip().split(": ") 
-        hashedPw = userTuple[1]
-        if bcrypt.checkpw(masterPassword.encode('utf-8'), hashedPw.encode('utf-8')):
-            return "Signed in"   
+        encryptedPw = userTuple[1]
+
+        if validatePassword(encryptedPw, masterPassword, KEY):
+            return "User validated"   
         else:
-            raise ValueError("Username or password is incorrect")
+            raise ValueError("Username or master password is incorrect")
     else:
-        raise ValueError("Username or password is incorrect")
+        raise ValueError("Username or master password is incorrect")
+
+def validatePassword(encryptedPw, providedPw, key):
+    fernet = Fernet(KEY)
+    decryptedPw = fernet.decrypt(encryptedPw)
+    return decryptedPw.decode('utf-8') == providedPw
+
+def decryptPassword(encryptedPw):
+    fernet = Fernet(KEY)
+    return fernet.decrypt(encryptedPw)    
+
+def encryptPassword(password):
+    cipherSuite = Fernet(KEY)
+    cipherText = cipherSuite.encrypt(password.encode("utf-8"))
+    return cipherText
+
+def savePassword(username, masterPassword, platform, password):
+    validateUser(username, masterPassword)
+    with open(username.lower() + ".txt") as file:
+        for line in file:
+            if platform.lower() in line:
+                raise ValueError("Platform already exists!")
+    bytes = encryptPassword(password)
+    platformEntry = platform.lower() + ": "
+    entry = platformEntry + bytes.decode('utf-8') + "\n"
+    f = open(username + ".txt", "a")
+    f.write(entry)
+    f.close()
+    return f"Password for '{platform.lower()}' successfully generated"
+
+def retrievePassword(username, masterPassword, platform):
+    validateUser(username, masterPassword)
+    filename = username.lower() + ".txt"
+    if os.path.isfile(filename):
+        match = ""
+        with open(filename) as file:
+            for line in file:
+                if platform.lower() in line:
+                    match = line
+            
+        if match != "":
+            platformTuple = match.strip().split(": ") 
+            encryptedPw = platformTuple[1]
+            return f"Your password for '{platform}' is '{decryptPassword(encryptedPw).decode('utf-8')}'"
+
+        else:
+            raise ValueError("No password associated with given platform")
+
+    else:
+        raise ValueError("No account associated with username, please create an account to proceed")
 
 
-signIn("Aero", "AeroThunder02")
+print(retrievePassword("aero", "AeroThunder02", "Steam"))
